3d3b04fdf3984d230724752d49fe0a0294a7b3e2
Norm2782 <Norm2782@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-01-25 21:19:57 +0000

    Minor fixes in docblock and coding standards. Added initial incubator documentation.
    git-svn-id: https://zym.googlecode.com/svn/trunk@5 242cd0b0-a150-0410-b6ef-417cb531347b

--- incubator/library/Zym/Controller/Action/Abstract.php
+++ incubator/library/Zym/Controller/Action/Abstract.php
@@ -6,9 +6,6 @@
  *
  * This source file is subject to the new BSD license that is bundled
  * with this package in the file LICENSE.txt.
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to contact@intermentis.nl so we can send you a copy immediately.
  *
  * @category   Zym
  * @package    Controller
@@ -54,13 +51,14 @@ abstract class Zym_Controller_Action_Abstract extends Zend_Controller_Action
     /**
      * Perform a redirect to an action/controller/module with params.
      *
-     * @param  string $action
-     * @param  string $controller
-     * @param  string $module
-     * @param  array $params
+     * @param string $action
+     * @param string $controller
+     * @param string $module
+     * @param array $params
+     * @param boolean $exit
      * @return void
      */
-    protected function _goto($action, $controller = null, $module = null, array $params = array(), $exit = true)
+    protected function goto($action, $controller = null, $module = null, array $params = array(), $exit = true)
     {
         if ($exit) {
             $this->_helper->redirector->gotoAndExit($action, $controller, $module, $params);
