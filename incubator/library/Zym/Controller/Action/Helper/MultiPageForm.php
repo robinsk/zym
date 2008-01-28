ca603e96ee4fa90b84fc9e0148dc8c89aa7860bd
Norm2782 <Norm2782@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-01-28 10:26:35 +0000

    Added initial Zym_Controller_Action_Helper_MultiPageForm
    git-svn-id: https://zym.googlecode.com/svn/trunk@8 242cd0b0-a150-0410-b6ef-417cb531347b

---
+++ incubator/library/Zym/Controller/Action/Helper/MultiPageForm.php
@@ -0,0 +1,587 @@
+<?php
+/**
+ * Zym
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @category   Zym
+ * @package    Controller
+ * @subpackage ActionHelper
+ * @copyright Copyright (c) 2008 Zym. (http://www.assembla.com/wiki/show/zym)
+ * @license http://www.assembla.com/wiki/show/dpEKouT5Gr3jP5abIlDkbG/License    New BSD License
+ */
+
+/**
+ * @see Zend_Session_Namespace
+ */
+require_once 'Zend/Session/Namespace.php';
+
+/**
+ * @see Zend_Controller_Action_Helper_Abstract
+ */
+require_once 'Zend/Controller/Action/Helper/Abstract.php';
+
+/**
+ * @category   Zym
+ * @package    Controller
+ * @subpackage ActionHelper
+ * @copyright Copyright (c) 2008 Zym. (http://www.assembla.com/wiki/show/zym)
+ * @license http://www.assembla.com/wiki/show/dpEKouT5Gr3jP5abIlDkbG/License    New BSD License
+ */
+class Zym_Controller_Action_Helper_MultiPageForm extends Zend_Controller_Action_Helper_Abstract
+{
+    /** 
+     * Zend_Session storage object
+     *
+     * @var Zend_Session
+     */ 
+    protected $_session = null; 
+ 
+    /**
+     * Default keys for controlling the multiform
+     * 
+     */ 
+    const ACTION_PREFIX     = '_'; 
+    const ACTION_KEY_NEXT   = '_next'; 
+    const ACTION_KEY_BACK   = '_back'; 
+    const ACTION_KEY_SUBMIT = '_submit'; 
+    const ACTION_KEY_CANCEL = '_cancel'; 
+    
+    /**
+     * The complete Zend_Form instance
+     *
+     * @var Zend_Form
+     */
+    protected $_form = null;
+    
+    /**
+     * The current subform instance
+     *
+     * @var Zend_Form
+     */
+    protected $_currentSubForm = null;
+    
+    /**
+     * The form data
+     *
+     * @var array
+     */
+    protected $_formData = array();
+    
+    /**
+     * The form action in order of apearance
+     *
+     * @var array
+     */
+    protected $_actions = array();
+    
+    /**
+     * The action that will be used for processing the form
+     *
+     * @var string
+     */
+    protected $_processAction = 'process';
+    
+    /**
+     * The action for canceling the form
+     *
+     * @var string
+     */
+    protected $_cancelAction = null;
+    
+    /**
+     * The last valid action
+     *
+     * @var string
+     */
+    protected $_validAction = null;
+    
+    /**
+     * Construct and set default session object
+     */ 
+    public function __construct()
+    {
+        $this->_session = new Zend_Session_Namespace($this->getName());
+    }
+    
+    /**
+     * Executed at preDispatch
+     *
+     */
+    public function preDispatch()
+    {
+        if (empty($this->_actions)) {
+            $this->_throwException('Multiform has not been assigned any actions');
+        } 
+        
+        $action = $this->getRequest()->getActionName(); 
+        
+        if ($action == $this->_processAction && !$this->isValid()) {
+            $this->redirect($this->getCurrentValidAction());
+        }
+        
+        if ($this->isSubformAction($action)) {
+            if (!$this->isValidAction($action)) {
+                $this->redirect($this->getCurrentValidAction());
+            } else {
+                $this->handle();
+            }
+            
+            $this->getActionController()->view->form = $this->getCurrentSubForm();
+        }
+    }
+    
+    /**
+     * Get the subform data
+     *
+     * @return array
+     */
+    public function getSubformData()
+    {
+        $formData = array();
+        
+        $forms = $this->_form->getSubForms();
+        
+        foreach ($forms as $form) {
+            $formData[] = $form->getValues();
+        }
+        
+        return $formData;
+    }
+
+    /**
+     * Determine if an action has been validated
+     * 
+     * @param string $current
+     * @return string
+     */ 
+    public function isValidAction($current)
+    {
+        foreach ($this->_actions as $action) { 
+            $this->_validAction = $action;
+            
+            if ($current == $action) { 
+                break;
+            }
+ 
+            if (!$this->isCompleteAction($action)) { 
+                return false;
+            }
+        }
+        
+        return true;
+    }
+ 
+    /**
+     * Determine if an action has been submitted
+     * 
+     * @param string $action
+     * @return mixed
+     */ 
+    public function isCompleteAction($action) 
+    {
+        if (isset($this->_session->valid[$action])) { 
+            return $this->_session->valid[$action]; 
+        }
+ 
+        return false; 
+    }
+    
+    /**
+     * Set the action used for processing the complete form
+     *
+     * @param string $action
+     * @return Zym_Controller_Action_Helper_MultiForm
+     */
+    public function setProcessAction($action)
+    {
+        $this->_processAction = $action;
+        
+        return $this;
+    }
+    
+    /**
+     * Get the processing action
+     *
+     * @return unknown
+     */
+    public function getProcessAction()
+    {
+        return $this->_processAction;
+    }
+    
+    /**
+     * Set a custom cancel action
+     *
+     * @param string $action
+     * @return Zym_Controller_Action_Helper_MultiForm
+     */
+    public function setCancelAction($action)
+    {
+        $this->_cancelAction = $action;
+        
+        return $this;
+    }
+    
+    /**
+     * Get the custom cancel action
+     *
+     * @return string
+     */
+    public function getCancelAction()
+    {
+        return $this->_cancelAction;
+    }
+    
+    /**
+     * Set sequence of actions
+     * 
+     * @param array $actions
+     * @return Zym_Controller_Action_Helper_MultiForm
+     */ 
+    public function setActions(array $actions) 
+    { 
+        $this->_actions = $actions; 
+ 
+        if (is_null($this->_session->valid) || !array_key_exists($actions[0], $this->_session->valid)) { 
+            $this->clear(); 
+        }
+ 
+        return $this; 
+    } 
+ 
+    /**
+     * Set values for an action
+     * 
+     * @param Zend_Form $values
+     * @param boolean $valid
+     * @return Zym_Controller_Action_Helper_MultiForm
+     */ 
+    public function setValues(Zend_Form $form, $valid = false) 
+    {
+        $action = $form->getName();
+        
+        if (!$this->isSubformAction($action)) { 
+            $this->_throwException(sprintf('"%s" is not a valid action', $action));
+        }
+        
+        $formValues = $form->getValues();
+        $formKeys = array_keys($formValues);
+        
+        foreach ($formKeys as $key) {
+            if (strpos($key, self::ACTION_PREFIX) === 0) {
+                unset($formValues[$key]);
+            }
+        }
+        
+        $this->_session->valid[$action] = (boolean) $valid; 
+        $this->_session->value[$action] = $formValues; 
+         
+        return $this; 
+    } 
+ 
+    /**
+     * Retrieve action values
+     * 
+     * @param string $action
+     * @return mixed
+     */ 
+    public function getValues($action = null) 
+    { 
+        if ($action === null) { 
+            return $this->_session->value; 
+        }
+ 
+        if (isset($this->_session->value[$action])) { 
+            return $this->_session->value[$action]; 
+        } 
+ 
+        return array(); 
+    } 
+ 
+    /**
+     * Retrieve current valid action
+     * 
+     * @return string
+     */ 
+    public function getCurrentValidAction() 
+    { 
+        return $this->_validAction; 
+    }
+    
+    /**
+     * Set a form instance
+     *
+     * @param Zend_Form $form
+     * @return Zym_Controller_Action_Helper_MultiForm
+     */
+    public function setForm(Zend_Form $form)
+    {
+        $this->_form = $form;
+        
+        $subForms = $form->getSubForms();
+        
+        foreach ($subForms as $subForm) {
+            $formName = $subForm->getName();
+             
+            if (empty($formName)) {
+                $this->_throwException('A subform needs to have a name.');
+            }
+        }
+        
+        return $this;
+    }
+    
+    /**
+     * Get the form instance
+     *
+     * @return Zend_Form
+     */
+    public function getForm()
+    {
+        if (!$this->_form) {
+            $this->_throwException('No form instance set.');
+        }
+        
+        return $this->_form;
+    }
+    
+    /**
+     * Get the current subform
+     *
+     * @return Zend_Form
+     */
+    public function getCurrentSubForm()
+    {
+        if (!$this->_currentSubForm) {            
+            $formName = $this->getRequest()->getActionName();
+            
+            $this->_currentSubForm = $this->getSubForm($formName);
+        }
+        
+        return $this->_currentSubForm;
+    }
+    
+    /**
+     * Get a subform by name.
+     *
+     * @param string $formName
+     * @return Zend_Form
+     */
+    public function getSubForm($formName)
+    {
+        $subForm = $this->_form->getSubForm($formName);
+        
+        if (empty($subForm)) {
+            $this->_throwException(sprintf('No form by the name of "%s" was registered.', $formName));
+        }
+        
+        $subForm->populate($this->getValues($formName));
+        
+        return $subForm;
+    }
+    
+    /**
+     * Check if the entire form is valid
+     *
+     * @return boolean
+     */
+    public function isValid()
+    {
+        $formData = $this->getAllSubformData();
+        
+        return $this->_form->isValid($formData);
+    }
+    
+    /**
+     * Get all data from the subforms
+     *
+     * @return array
+     */
+    public function getAllSubformData()
+    {
+        $formData = array();
+        
+        foreach ($this->_actions as $action) {
+            $formData[$action] = $this->getValues($action);
+        }
+        
+        return $formData;
+    }
+    
+    /**
+     * Get the form data. If it's empty and a submitted form is available,
+     * populate it first from POST.
+     *
+     * @return array
+     */
+    public function getFormData()
+    {
+        if (empty($this->_formData) && $this->getRequest()->isPost()) {
+            $postData = $this->getRequest()->getPost();
+            
+            $elements = $this->getCurrentSubForm()->getElements();
+            
+            $this->_formData = array_intersect_key($postData, $elements);
+        }
+        
+        return $this->_formData;
+    }
+    
+    /**
+     * Use the redirector helper to navigate the controller
+     * 
+     * @param string $action
+     * @param string $controller
+     * @param string $module
+     */ 
+    public function redirect($action, $controller = null, $module = null) 
+    { 
+        $redirector = $this->getActionController()->getHelper('Redirector');
+        
+        return $redirector->gotoAndExit($action, 
+                                        $controller, 
+                                        $module); 
+    }
+    
+    /**
+     * Get the action used to submit the form
+     *
+     * @return string|false
+     */
+    public function getSubmitAction() 
+    {
+        $formData = $this->getFormData();
+        
+        if (!empty($formData)) {
+            $formDataKeys = array_keys($formData);
+            
+            foreach ($formDataKeys as $key) {
+                if (strpos($key, self::ACTION_PREFIX) === 0) {
+                    return $key; 
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    /**
+     * Handle the form
+     *
+     * @return boolean
+     */
+    public function handle()
+    {
+        $action = $this->getSubmitAction();
+        
+        if ($action === false) {
+            return false;
+        }
+        
+        $currentSubForm = $this->getCurrentSubForm();
+        $valid = $currentSubForm->isValid($this->getFormData());
+        
+        $this->setValues($currentSubForm, $valid);
+        
+        switch ($action) {
+            case self::ACTION_KEY_BACK:
+                $position = array_search($currentSubForm->getName(), $this->_actions);
+                
+                if ($position <= 0) {
+                    $action = $this->_actions[0];
+                } else {
+                    $action = $this->_actions[$position - 1]; 
+                }
+                break;
+                
+            case self::ACTION_KEY_NEXT:
+                if (!$valid) { 
+                    return false; 
+                } 
+                
+                $position = array_search($currentSubForm->getName(), $this->_actions);
+                
+                if ($position == count($this->_actions) - 1) { 
+                    $action = $this->_actions[count($this->_actions) - 1]; 
+                } else { 
+                    $action = $this->_actions[$position + 1]; 
+                }
+                break;
+                
+            case self::ACTION_KEY_SUBMIT;
+                if (!$valid) { 
+                    return false; 
+                }
+                
+                $action = $this->getProcessAction();
+                break;
+                
+            case self::ACTION_KEY_CANCEL:
+                if (empty($this->_cancelAction)) {
+                    $this->clear();
+                    $action = $this->getCurrentValidAction();
+                } else {
+                    $action = $this->getCancelAction();
+                }
+                break;
+                
+            default:
+                if (!in_array($action, $this->_actions)) {
+                    $this->redirect($this->getCurrentValidAction()); 
+                }
+                break;
+        }
+        
+        return $this->redirect($action);
+    }
+    
+    /**
+     * Check if the action is an action for this form.
+     *
+     * @param string $action
+     * @return boolean
+     */
+    public function isSubformAction($action)
+    {
+        return in_array($action, $this->_actions);
+    }
+    
+    /**
+     * Reset all session data
+     */ 
+    public function clear() 
+    { 
+        $this->_default = array(); 
+
+        $this->_session->valid = array(); 
+        $this->_session->value = array(); 
+        $this->_session->registry = array(); 
+ 
+        foreach ($this->_actions as $id) { 
+            if (!isset($this->_session->valid[$id])) { 
+                $this->_session->valid[$id] = false; 
+                $this->_session->value[$id] = array(); 
+            } 
+        } 
+    } 
+    
+    /**
+     * Throw an exception
+     *
+     * @param string $message
+     * @throws Zym_Controller_Exception
+     */
+    protected function _throwException($message)
+    {
+        /**
+         * @see Zym_Controller_Exception
+         */
+        require_once 'Zym/Controller/Exception.php';
+        
+        throw new Zym_Controller_Exception($message);
+    }
+}
