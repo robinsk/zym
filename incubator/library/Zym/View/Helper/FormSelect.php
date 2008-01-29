d0b4b5f9a7699335a80acf8dad59ffc5a5678d6f
Norm2782 <Norm2782@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-01-29 18:27:37 +0000

    Added 'Zym_View_Helper_FormSelect'
    git-svn-id: https://zym.googlecode.com/svn/trunk@15 242cd0b0-a150-0410-b6ef-417cb531347b

---
+++ incubator/library/Zym/View/Helper/FormSelect.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_View
+ * @subpackage Helper
+ * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+
+/**
+ * Abstract class for extension
+ */
+require_once 'Zend/View/Helper/FormElement.php';
+
+
+/**
+ * Helper to generate "select" list of options
+ *
+ * @category   Zend
+ * @package    Zend_View
+ * @subpackage Helper
+ * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class Zym_View_Helper_FormSelect extends Zend_View_Helper_FormElement
+{
+    /**
+     * Generates 'select' list of options.
+     *
+     * @access public
+     *
+     * @param string|array $name If a string, the element name.  If an
+     * array, all other parameters are ignored, and the array elements
+     * are extracted in place of added parameters.
+     *
+     * @param mixed $value The option value to mark as 'selected'; if an
+     * array, will mark all values in the array as 'selected' (used for
+     * multiple-select elements).
+     *
+     * @param array|string $attribs Attributes added to the 'select' tag.
+     *
+     * @param array $options An array of key-value pairs where the array
+     * key is the radio value, and the array value is the radio text.
+     *
+     * @param array An array with the name of items that need to be disabled.
+     *
+     * @param string $listsep When disabled, use this list separator string
+     * between list values.
+     *
+     * @return string The select tag and options XHTML.
+     */
+    public function formSelect($name, $value = null, $attribs = null,
+        $options = null, $disabled = null, $listsep = "<br />\n")
+    {
+        $id = '';
+        $disable = false;
+
+        $info = $this->_getInfo($name, $value, $attribs, $options, $listsep);
+        extract($info); // name, id, value, attribs, options, listsep, disable
+
+        // force $value to array so we can compare multiple values
+        // to multiple options.
+        settype($value, 'array');
+        settype($disabled, 'array');
+
+        $isArrayElement = (substr($name, -2) == '[]');
+
+        // check for multiple attrib and change name if needed
+        if (isset($attribs['multiple']) &&
+            $attribs['multiple'] == 'multiple' &&
+            !$isArrayElement) {
+            $name .= '[]';
+        }
+
+        // check for multiple implied by the name and set attrib if
+        // needed
+        if ($isArrayElement) {
+            $attribs['multiple'] = 'multiple';
+        }
+
+        // now start building the XHTML.
+        if ($disable) {
+
+            // disabled.
+            // generate a plain list of selected options.
+            // show the label, not the value, of the option.
+            $list = array();
+            foreach ((array) $options as $optValue => $optLabel) {
+                if (in_array($optValue, $value, 0 === $optValue)) {
+                    // add the hidden value
+                    $option = $this->_hidden($name, $optValue);
+                    // add the display label
+                    $option .= $this->view->escape($optLabel);
+                    // add to the list
+                    $list[] = $option;
+                }
+            }
+            $xhtml = implode($listsep, $list);
+
+        } else {
+
+            // enabled.
+            // the surrounding select element first.
+            $xhtml = '<select'
+                   . sprintf(' name="%s"', $this->view->escape($name))
+                   . sprintf(' id="%s"', $this->view->escape($id))
+                   . $this->_htmlAttribs($attribs)
+                   . ">\n\t";
+
+            // build the list of options
+            $list = array();
+            foreach ((array) $options as $optValue => $optLabel) {
+
+                if (is_array($optLabel)) {
+                    $list[] = '<optgroup '
+                            . sprintf('label="%s">',  $this->view->escape($optValue));
+                    foreach ($optLabel as $val => $lab) {
+                        $list[] = $this->_build($val, $lab, $value, $disabled);
+                    }
+                    $list[] = '</optgroup>';
+                } else {
+                    $list[] = $this->_build($optValue, $optLabel, $value, $disabled);
+                }
+            }
+
+            // add the options to the xhtml and close the select
+            $xhtml .= implode("\n\t", $list) . "\n</select>";
+
+        }
+
+        return $xhtml;
+    }
+
+    /**
+     * Builds the actual <option> tag
+     *
+     * @param string $value Options Value
+     * @param string $label Options Label
+     * @param array  $selected The option value(s) to mark as 'selected'
+     * @param array  $disabled The option value(s) to mark as 'disabled'
+     * @return string Option Tag XHTML
+     */
+    protected function _build($value, $label, $selected, $disabled)
+    {
+        $option = '<option'
+             . sprintf(' value="%s"', $this->view->escape($value))
+             . sprintf(' label="%s"', $this->view->escape($label));
+
+        // selected?
+        if (in_array($value, $selected, 0 === $value)) {
+            $option .= ' selected="selected"';
+        }
+
+        // disabled?
+        if (in_array($value, $disabled, 0 === $value)) {
+            $option .= ' disabled="disabled"';
+        }
+
+        $option .= sprintf('>%s</option>', $this->view->escape($label));
+
+        return $option;
+    }
+
+}
