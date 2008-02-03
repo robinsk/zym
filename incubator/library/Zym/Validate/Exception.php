76fc6acab293c92038a0f8cae41537f7819d1d6b
SpotSec <SpotSec@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-02-03 05:57:21 +0000

    Added Zym_Validate Added Zym_Validate_StringEquals -- Is it really needed?
    git-svn-id: https://zym.googlecode.com/svn/trunk@32 242cd0b0-a150-0410-b6ef-417cb531347b

---
+++ incubator/library/Zym/Validate/Exception.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Zym Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @category Zym
+ * @package Zym_Validate
+ * @copyright Copyright (c) 2008 Zym. (http://www.assembla.com/wiki/show/zym)
+ * @license http://www.assembla.com/wiki/show/zym/License New BSD License
+ */
+
+/**
+ * @see Zym_Exception
+ */
+require_once 'Zym/Exception';
+
+/**
+ * Validate exception class
+ *
+ * @author Geoffrey Tran
+ * @license http://www.assembla.com/wiki/show/zym/License New BSD License
+ * @category Zym
+ * @package Zym_Validate
+ * @copyright Copyright (c) 2008 Zym. (http://www.assembla.com/wiki/show/zym)
+ */
+class Zym_Validate_Exception extends Zym_Exception
+{
+}
