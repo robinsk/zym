746295bb5368b1cc2dfe5ad196f2b54b08f87911
SpotSec <SpotSec@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-02-03 20:31:48 +0000

    phpdoc changes
    git-svn-id: https://zym.googlecode.com/svn/trunk@37 242cd0b0-a150-0410-b6ef-417cb531347b

--- incubator/library/Zym/Controller/Action/Exception/Interface.php
+++ incubator/library/Zym/Controller/Action/Exception/Interface.php
@@ -8,8 +8,8 @@
  * with this package in the file LICENSE.txt.
  *
  * @category Zym
- * @package Zym_Controller_Action
- * @subpackage Exception
+ * @package Zym_Controller
+ * @subpackage Action_Exception
  * @copyright Copyright (c) 2008 Zym. (http://www.assembla.com/wiki/show/zym)
  * @license http://www.assembla.com/wiki/show/zym/License New BSD License
  */
@@ -18,8 +18,8 @@
  * @author Geoffrey Tran
  * @license http://www.assembla.com/wiki/show/zym/License New BSD License
  * @category Zym
- * @package Zym_Controller_Action
- * @subpackage Exception
+ * @package Zym_Controller
+ * @subpackage Action_Exception
  * @copyright Copyright (c) 2008 Zym. (http://www.assembla.com/wiki/show/zym)
  */
 interface Zym_Controller_Action_Exception_Interface
