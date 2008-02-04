f2a9ba92186cb0655d8ddad0bb69b7bf2a1d72e7
SpotSec <SpotSec@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-02-04 23:39:50 +0000

    Added Zym_Session_Validator_Cookie Added Zym_Session_Validator_RemoteAddr
    git-svn-id: https://zym.googlecode.com/svn/trunk@50 242cd0b0-a150-0410-b6ef-417cb531347b

---
+++ incubator/library/Zym/Session/Validator/RemoteAddr.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Zym Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @category Zym
+ * @package Zym_Session
+ * @subpackage Validator
+ * @copyright Copyright (c) 2008 Zym. (http://www.assembla.com/wiki/show/zym)
+ * @license http://www.assembla.com/wiki/show/zym/License New BSD License
+ */
+
+/**
+ * @see Zend_Session_Validator_Abstract
+ */
+require_once 'Zend/Session/Validator/Abstract.php';
+
+/**
+ * @see Zend_Session_Validator_Interface
+ */
+require_once 'Zend/Session/Validator/Interface.php';
+
+/**
+ * Validate a session using a user's IP address
+ * 
+ * @todo expand this component to handle HTTP_X_FORWARDED_FOR etc...
+ *
+ * @author Geoffrey Tran
+ * @license http://www.assembla.com/wiki/show/zym/License New BSD License
+ * @category Zym
+ * @package Zym_Session
+ * @subpackage Validator
+ * @copyright Copyright (c) 2008 Zym. (http://www.assembla.com/wiki/show/zym)
+ */
+class Zym_Session_Validator_RemoteAddr extends Zend_Session_Validator_Abstract implements Zend_Session_Validator_Interface 
+{   
+    /**
+     * Setup the validator
+     *
+     * Implement setup()
+     */
+    public function setup()
+    {
+        $remoteAddr = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null;
+        $this->setValidData($remoteAddr);
+    }
+    
+    /**
+     * Validation process
+     *
+     * Implement validate()
+     * 
+     * @return boolean
+     */
+    public function validate()
+    {
+        $remoteAddr     = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null;
+        $lastRemoteAddr = $this->getValidData();
+
+        if ($remoteAddr == $lastRemoteAddr) {
+            return true;
+        }
+        
+        return false;
+    }
+}
