2e2af040bc1535e9e088b56d77971c1ca72ef9ed
Norm2782 <Norm2782@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-02-16 12:07:56 +0000

    Added docblocks Requires Zym_Db_Table_Abstract to work properly
    git-svn-id: https://zym.googlecode.com/svn/trunk@91 242cd0b0-a150-0410-b6ef-417cb531347b

--- incubator/library/Zym/View/Helper/ViewRow.php
+++ incubator/library/Zym/View/Helper/ViewRow.php
@@ -1,17 +1,43 @@
 <?php
+/**
+ * Zym
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @author     Jurrien Stutterheim
+ * @category   Zym_View
+ * @package    Helper
+ * @copyright  Copyright (c) 2008 Zym. (http://www.assembla.com/wiki/show/zym)
+ * @license http://www.assembla.com/wiki/show/dpEKouT5Gr3jP5abIlDkbG/License    New BSD License
+ */
+
+/**
+ * @author     Jurrien Stutterheim
+ * @category   Zym_View
+ * @package    Helper
+ * @copyright  Copyright (c) 2008 Zym. (http://www.assembla.com/wiki/show/zym)
+ * @license http://www.assembla.com/wiki/show/dpEKouT5Gr3jP5abIlDkbG/License    New BSD License
+ */
 class Zym_View_Helper_ViewRow
 {
+    /**
+     * Render the Zend_Db_Table_Row_Abstract in a table
+     *
+     * @param Zend_Db_Table_Row_Abstract $row
+     * @return string
+     */
     public function viewRow(Zend_Db_Table_Row_Abstract $row)
     {
-        $tableInfo = $row->getTable()->info();
-        $metaData = $tableInfo[Zend_Db_Table_Abstract::METADATA];
-
+        $table = $row->getTable();
         $rowData = $row->toArray();
 
         $xhtml = '<table>';
 
         foreach ($rowData as $key => $value) {
-            if (!(bool) $metaData[$key]['IDENTITY']) {
+            if (!$table->isIdentity($key)) {
                 $xhtml .= '<tr>';
 
                 $xhtml .= sprintf('<td><strong>%s</strong></td>', $key);
