453aab24635181f9932203150a2e55f1be8ef4ff
Norm2782 <Norm2782@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-02-19 08:05:57 +0000

    Added ArrayData_Interface Removed FormSelect helper
    git-svn-id: https://zym.googlecode.com/svn/trunk@96 242cd0b0-a150-0410-b6ef-417cb531347b

---
+++ incubator/library/Zym/ArrayData/Interface.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Zym
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @author     Jurrien Stutterheim
+ * @category   Zym_ArrayData
+ * @copyright  Copyright (c) 2008 Zym. (http://www.assembla.com/wiki/show/zym)
+ * @license http://www.assembla.com/wiki/show/dpEKouT5Gr3jP5abIlDkbG/License    New BSD License
+ */
+
+/**
+ * @author     Jurrien Stutterheim
+ * @category   Zym_ArrayData
+ * @copyright  Copyright (c) 2008 Zym. (http://www.assembla.com/wiki/show/zym)
+ * @license http://www.assembla.com/wiki/show/dpEKouT5Gr3jP5abIlDkbG/License    New BSD License
+ */
+interface Zym_ArrayData_Interface
+{
+    /**
+     * Return an array
+     *
+     * @return array
+     */
+    public function toArray();
+
+    /**
+     * Load data from an array
+     *
+     * @param array $data
+     */
+    public function setFromArray(array $data);
+}
