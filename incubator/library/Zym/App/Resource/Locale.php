f3fd94ee2aff4bbeb151f5e7f8a017bf9bd42c50
SpotSec <SpotSec@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-03-02 06:37:20 +0000

    locale resource
    git-svn-id: https://zym.googlecode.com/svn/trunk@151 242cd0b0-a150-0410-b6ef-417cb531347b

---
+++ incubator/library/Zym/App/Resource/Locale.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Zym Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @category Zym
+ * @package Zym_App
+ * @subpackage Resource
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license http://www.zym-project.com/license New BSD License
+ */
+
+/**
+ * @see Zym_App_Resource_Abstract
+ */
+require_once 'Zym/App/Resource/Abstract.php';
+
+/**
+ * @see Zym_Cache
+ */
+require_once 'Zym/Cache.php';
+
+/**
+ * @see Zend_Locale
+ */
+require_once 'Zend/Locale.php';
+
+/**
+ * @see Zend_Loader
+ */
+require_once 'Zend/Loader.php';
+
+/**
+ * Setup default locale
+ * 
+ * @author Geoffrey Tran
+ * @license http://www.zym-project.com/license New BSD License
+ * @category Zym
+ * @package Zym_App
+ * @subpackage Resource
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ */
+class Zym_App_Resource_Locale extends Zym_App_Resource_Abstract
+{   
+    /**
+     * Priority
+     *
+     * @var integer
+     */
+    protected $_priority = self::PRIORITY_HIGH;
+    
+    /**
+     * Default Config
+     *
+     * @var array
+     */
+    protected $_defaultConfig = array(
+        Zym_App::ENV_DEVELOPMENT => array(
+            'cache' => false
+        ),
+    
+        Zym_App::ENV_DEFAULT => array(
+            'class' => 'Zend_Locale',
+            'cache' => true,
+            'default' => null
+        )
+    );
+
+    /**
+     * PreSetup
+     *
+     * @param Zend_Config $config
+     */
+    public function preSetup(Zend_Config $config)
+    {
+        // Load locale class
+        $this->_loadLocaleClass($config->class);
+    }
+    
+    /**
+     * Setup
+     *
+     * @param Zend_Config $config
+     */
+    public function setup(Zend_Config $config)
+    {   
+        // Set cache
+        $this->_setCache($config);
+        
+        // Set default locale
+        $this->_setDefault($config);
+    }
+    
+    /**
+     * Load locale class
+     *
+     * @param string $class
+     */
+    protected function _loadLocaleClass($class)
+    {
+        Zend_Loader::loadClass($class);
+        $this->_class = $class;
+    }
+    
+    /**
+     * Set cache
+     *
+     * @param Zend_Config $config
+     */
+    protected function _setCache(Zend_Config $config)
+    {
+        if (!$config->cache) {
+            return;
+        }
+
+        $cache = Zym_Cache::factory('Core');
+        call_user_func(array($this->_class, 'setCache'), $cache);
+    }
+    
+    /**
+     * Set default locale
+     *
+     * @param Zend_Config $config
+     */
+    protected function _setDefault(Zend_Config $config)
+    {
+        $locale = $config->default;
+        call_user_func(array($this->_class, 'setDefault'), $locale);
+    }
+}
