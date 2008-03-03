35b3a7f5457a6f920154f27b37fdbb5fa52feded
Norm2782 <Norm2782@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-03-03 21:46:02 +0000

    Added initial FormLoader and ModelLoader components. note: they're just brainstorms atm.
    git-svn-id: https://zym.googlecode.com/svn/trunk@161 242cd0b0-a150-0410-b6ef-417cb531347b

---
+++ incubator/library/Zym/Loader/Abstract.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Zym
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @author     Jurrien Stutterheim
+ * @category   Zym
+ * @package    Zym_Loader
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license    http://www.zym-project.com/license    New BSD License
+ */
+
+/**
+ * @author     Jurrien Stutterheim
+ * @category   Zym
+ * @package    Zym_Loader
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license    http://www.zym-project.com/license    New BSD License
+ */
+abstract class Zym_Loader_Abstract
+{
+    /**
+     * @var string
+     */
+    protected $_modelDirectory = null;
+
+    /**
+     * @var string
+     */
+    protected $_modelPrefix = null;
+
+    /**
+     * @var Zend_Controller_Dispatcher_Standard
+     */
+    protected $_dispatcher = null;
+
+    /**
+     * @var Zend_Controller_Request
+     */
+    protected $_request = null;
+
+    /**
+     * Constructor.
+     * Set dispatcher and request
+     */
+    public function __construct()
+    {
+        if (!$this->_modelDirectory) {
+            throw new Exception('Model directory must be set.');
+        }
+
+        $frontController = Zend_Controller_Front::getInstance();
+        $this->_dispatcher = $frontController->getDispatcher();
+        $this->_request = $frontController->getRequest();
+    }
+
+    /**
+     * Load the model
+     *
+     * @throws Exception
+     * @param string $modelName
+     * @param string $modelPrefix
+     * @param string $module
+     */
+    public function loadModel($modelName, $modelPrefix = null, $module = null)
+    {
+        $modelName = ucfirst($modelName);
+
+        if (!$module) {
+            $module = $this->_request->getModuleName();
+        }
+
+        if (!$modelPrefix) {
+            $modelPrefix = $this->_modelPrefix;
+        }
+
+        if ($modelPrefix) {
+            $modelName = ucfirst($modelPrefix) . '/' . $modelName;
+        }
+
+        $modelName =  str_ireplace('_', '/', $modelName);
+
+        $controllerDirectory = $this->_dispatcher->getControllerDirectory($module);
+        $moduleDirectory = dirname($controllerDirectory);
+
+        $filePath = array($moduleDirectory,
+                          $this->_modelDirectory,
+                          $modelName);
+
+        $file = implode('/', $filePath) . '.php';
+
+        if (file_exists($file)) {
+            require_once $file;
+        } else {
+            throw new Exception(sprintf('File "%s" could not be loaded', $file));
+        }
+    }
+}
---
+++ incubator/library/Zym/Loader/FormLoader.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Zym
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @author     Jurrien Stutterheim
+ * @category   Zym
+ * @package    Zym_Loader
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license    http://www.zym-project.com/license    New BSD License
+ */
+
+/**
+ * @see Zym_Loader_Abstract
+ */
+require_once 'Zym/Loader/Abstract.php';
+
+/**
+ * @author     Jurrien Stutterheim
+ * @category   Zym
+ * @package    Zym_Loader
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license    http://www.zym-project.com/license    New BSD License
+ */
+class Zym_Loader_FormLoader extends Zym_Loader_Abstract
+{
+    /**
+     * @var string
+     */
+    protected $_modelDirectory = 'forms';
+
+    /**
+     * @var Zym_Loader_FormLoader
+     */
+    protected static $_instance = null;
+
+    /**
+     * Singleton
+     *
+     * @return Zym_Loader_ModelLoader
+     */
+    protected static function getInstance()
+    {
+        if (!self::$_instance) {
+            self::$_instance = new self();
+        }
+
+        return self::$_instance;
+    }
+
+    /**
+     * Load the model
+     */
+    public static function load($formName, $modelPrefix = null, $module = null)
+    {
+        $formLoader = Zym_Loader_FormLoader::getInstance();
+        $formLoader->loadModel($formName, $modelPrefix, $module);
+    }
+}
