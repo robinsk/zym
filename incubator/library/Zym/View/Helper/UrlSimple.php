082812cd7dfed6d0927dde39aa63053c207dca41
SpotSec <SpotSec@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-04-10 22:07:54 +0000

    git-svn-id: https://zym.googlecode.com/svn/trunk@298 242cd0b0-a150-0410-b6ef-417cb531347b

---
+++ incubator/library/Zym/View/Helper/UrlSimple.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Zym Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @category Zym
+ * @package Zym_View
+ * @subpackage Helper
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license http://www.zym-project.com/license New BSD License
+ */
+
+/**
+ * @see Zend_Controller_Action_HelperBroker
+ */
+require_once 'Zend/Controller/Action/HelperBroker.php';
+
+/**
+ * @author Geoffrey Tran
+ * @license http://www.zym-project.com/license New BSD License
+ * @package Zym_View
+ * @subpackage Helper
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ */
+class Zym_View_Helper_UrlSimple
+{
+    /**
+     * Url action helper
+     *
+     * @var Zend_Controller_Action_Helper_Url
+     */
+    protected $_actionHelper;
+    
+    /**
+     * Create URL based on default route
+     *
+     * @todo consider copying code from the action helper to reduce loaded files
+     * 
+     * @param  string $action
+     * @param  string $controller
+     * @param  string $module
+     * @param  array $params
+     * @return string
+     */
+    public function urlSimple($action, $controller = null, $module = null, array $params = null)
+    {
+        if (!$this->_actionHelper instanceof Zend_Controller_Action_Helper_Url) {
+            $this->_actionHelper = Zend_Controller_Action_HelperBroker::getStaticHelper('url');
+        }
+        
+        return $this->_actionHelper->simple($action, $controller, $module, $params);
+    }
+}
