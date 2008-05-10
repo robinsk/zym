723ab1e2fb60d953629556a3595a7df35c46e67a
SpotSec <SpotSec@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-05-10 19:55:30 +0000

    View resource improvements Zym_Viev should be usable now
    git-svn-id: https://zym.googlecode.com/svn/trunk@318 242cd0b0-a150-0410-b6ef-417cb531347b

---
+++ incubator/library/Zym/App/Resource/View/Exception.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Zym Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @category Zym
+ * @package Zym_App
+ * @subpackage Resource_Mail
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license http://www.zym-project.com/license New BSD License
+ */
+
+
+/**
+ * @see Zym_App_Resource_Exception
+ */
+require_once 'Zym/App/Resource/Exception.php';
+
+/**
+ * @author Geoffrey Tran
+ * @license http://www.zym-project.com/license New BSD License
+ * @category Zym
+ * @package Zym_App
+ * @subpackage Resource_View
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ */
+class Zym_App_Resource_View_Exception extends Zym_App_Resource_Exception
+{}
