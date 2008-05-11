c863ad05811fae8cefa5d88840e9dd10ca33290a
Norm2782 <Norm2782@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-05-11 12:12:43 +0000

    Added the Zym_Paginate factory Added Zym_Paginate_Db Todo: unit tests
    git-svn-id: https://zym.googlecode.com/svn/trunk@319 242cd0b0-a150-0410-b6ef-417cb531347b

---
+++ library/Zym/Paginate.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Zym
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @author     Jurrien Stutterheim
+ * @category   Zym
+ * @package    Zym_Paginate
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license    http://www.zym-project.com/license    New BSD License
+ */
+
+/**
+ * @see Zym_Paginate_Array
+ */
+require_once 'Zym/Paginate/Array.php';
+
+/**
+ * @see Zym_Paginate_Db
+ */
+require_once 'Zym/Paginate/Db.php';
+
+/**
+ * @see Zym_Paginate_Iterator
+ */
+require_once 'Zym/Paginate/Iterator.php';
+
+/**
+ * @author     Jurrien Stutterheim
+ * @category   Zym
+ * @package    Zym_Paginate
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license    http://www.zym-project.com/license    New BSD License
+ */
+class Zym_Paginate
+{
+    /**
+     * Factory
+     *
+     * @param Zend_Db_Select|Iterator|array $data
+     * @return Zym_Paginate_Abstract
+     * @throws Zym_Paginate_Exception
+     */
+    public static function factory($data)
+    {
+        $paginate = null;
+
+        if ($data instanceof Zend_Db_Select) {
+            $paginate = new Zym_Paginate_Db($data);
+        } else if ($data instanceof Iterator) {
+            $paginate = new Zym_Paginate_Iterator($data);
+        } else if (is_array($data)) {
+            $paginate = new Zym_Paginate_Array($data);
+        } else {
+            $type = null;
+
+            if (is_object($data)) {
+                $type = get_class($data);
+            } else {
+                $type = gettype($data);
+            }
+
+            throw new Zym_Paginate_Exception('Can\'t paginate data of type "' . $type . '".');
+        }
+
+        return $paginate;
+    }
+}
---
+++ library/Zym/Paginate/Db.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Zym
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @author     Jurrien Stutterheim
+ * @category   Zym
+ * @package    Zym_Paginate
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license    http://www.zym-project.com/license    New BSD License
+ */
+
+/**
+ * @see Zym_Paginate_Abstract
+ */
+require_once 'Zym/Paginate/Abstract.php';
+
+/**
+ * @author     Jurrien Stutterheim
+ * @category   Zym
+ * @package    Zym_Paginate
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license    http://www.zym-project.com/license    New BSD License
+ */
+class Zym_Paginate_Db extends Zym_Paginate_Abstract
+{
+    /**
+     * The name for the rowcount column
+     */
+    const ROW_COUNT_COLUMN = 'zym_paginate_row_count';
+
+    /**
+     * @var Zend_Db_Table_Select
+     */
+    protected $_select = null;
+
+    /**
+     * Constructor
+     *
+     * @param Zend_Db_Table_Select $select
+     */
+    public function __construct(Zend_Db_Select $select)
+    {
+        $this->_select = $select;
+
+        // @TODO: Check how well this works when you have a query with multiple joined tables
+        $countSelect = clone $select;
+
+        $matches = null;
+        preg_match('/FROM `(.+)`/', (string) $select, $matches);
+        $countSelect->from($matches[1], new Zend_Db_Expr('COUNT(*) AS ' . self::ROW_COUNT_COLUMN));
+
+        $result = $countSelect->query()->fetchAll();
+
+        $this->_rowCount = (int) $result[0][self::ROW_COUNT_COLUMN];
+    }
+
+    /**
+     * Get a page
+     *
+     * @var int $page
+     * @return array
+     */
+    public function getPage($page)
+    {
+        $this->_select->limitPage((int) $page, $this->getRowLimit());
+
+        return $this->_select->query()->fetchAll();
+    }
+
+    /**
+     * Get the total amount of pages
+     *
+     * @return int
+     */
+    public function getPageCount()
+    {
+        if ($this->_pageCount === 0 && $this->_rowCount > 0) {
+            $rowCount = $this->getRowCount();
+
+            $floor = floor($rowCount / $this->_rowLimit);
+            $rest = $rowCount % $this->_rowLimit;
+
+            $this->_pageCount = $floor + ($rest > 0 ? 1 : 0);
+        }
+
+        return (int) $this->_pageCount;
+    }
+}
