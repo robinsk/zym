9722d7c2a98be2e17f1ae202a787740f34fd9794
SpotSec <SpotSec@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-05-28 00:11:45 +0000

    Tests structure
    git-svn-id: https://zym.googlecode.com/svn/trunk@343 242cd0b0-a150-0410-b6ef-417cb531347b

---
+++ incubator/tests/Zym/CacheSuite.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Zym
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @author     Geoffrey Tran
+ * @category   Zym_Tests
+ * @package    Zym_Cache
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license    http://www.zym-project.com/license    New BSD License
+ */
+
+/**
+ * @see PHPUnit_Framework_TestSuite
+ */
+require_once 'PHPUnit/Framework/TestSuite.php';
+
+/**
+ * TestSuite
+ *
+ * @author     Geoffrey Tran
+ * @category   Zym_Tests
+ * @package    Zym_Cache
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license    http://www.zym-project.com/license    New BSD License
+ */
+class Zym_CacheSuite extends PHPUnit_Framework_TestSuite
+{    
+    /**
+     * Construct
+     *
+     */
+    public function __construct()
+    {
+        $this->setName($this->_createName());
+        
+        $name     = substr($this->getName(), 0, -5);
+        $tests    = array(dirname(__FILE__) . "/{$name}Test.php");
+        $iterator = new RecursiveDirectoryIterator(dirname(__FILE__) . '/' . $name);
+        foreach(new RecursiveIteratorIterator($iterator) as $file) {
+            if ($file->isFile() && substr($file, -8) == 'Test.php') {
+                $tests[] = (string) $file;
+            }
+        }
+        
+        $this->addTestFiles($tests);
+    }
+    
+    /**
+     * Get suite
+     *
+     * @return PHPUnit_Framework_TestSuite
+     */
+    public static function suite()
+    {
+        return new self();
+    }
+    
+    /**
+     * Create name
+     *
+     * @return string
+     */
+    protected function _createName()
+    {
+        $parts = explode('_', get_class($this));
+        $name  = $parts[count($parts) - 1];
+        
+        return $name;
+    }
+}
