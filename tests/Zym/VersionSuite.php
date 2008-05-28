84ed56609d80f951e93b17a8ce088df4f8eea173
SpotSec <SpotSec@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-05-28 05:29:32 +0000

    More tests
    git-svn-id: https://zym.googlecode.com/svn/trunk@357 242cd0b0-a150-0410-b6ef-417cb531347b

---
+++ tests/Zym/VersionSuite.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Zym
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @author     Geoffrey Tran
+ * @category   Zym_Tests
+ * @package    Zym_Version
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license    http://www.zym-project.com/license    New BSD License
+ */
+
+/**
+ * @see PHPUnit_Framework_TestSuite
+ */
+require_once 'PHPUnit/Framework/TestSuite.php';
+
+/**
+ * TestSuite
+ *
+ * @author     Geoffrey Tran
+ * @category   Zym_Tests
+ * @package    Zym_Version
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license    http://www.zym-project.com/license    New BSD License
+ */
+class Zym_VersionSuite extends PHPUnit_Framework_TestSuite
+{    
+    /**
+     * Construct
+     *
+     */
+    public function __construct()
+    {
+        $this->setName($this->_createName());
+        
+        $name     = substr($this->getName(), 0, -5);
+        $tests    = array();
+        
+        $componentFile = dirname(__FILE__) . "/{$name}Test.php";
+        if (file_exists($componentFile)) {
+            $tests[] = $componentFile;
+        }
+        
+        $componentDir = dirname(__FILE__) . '/' . $name;
+        if (file_exists($componentDir)) {
+            $iterator = new RecursiveDirectoryIterator($componentDir);
+            foreach(new RecursiveIteratorIterator($iterator) as $file) {
+                if ($file->isFile() && substr($file, -8) == 'Test.php') {
+                    $tests[] = (string) $file;
+                }
+            }
+        }
+        
+        $this->addTestFiles($tests);
+    }
+    
+    /**
+     * Get suite
+     *
+     * @return PHPUnit_Framework_TestSuite
+     */
+    public static function suite()
+    {
+        return new self();
+    }
+    
+    /**
+     * Create name
+     *
+     * @return string
+     */
+    protected function _createName()
+    {
+        $parts = explode('_', get_class($this));
+        $name  = $parts[count($parts) - 1];
+        
+        return $name;
+    }
+}
---
+++ tests/Zym/VersionTest.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Zym Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @category Zym
+ * @package Zym_Version
+ * @copyright Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license http://www.zym-project.com//License New BSD License
+ */
+
+/**
+ * @see PHPUnit_Framework_TestCase
+ */
+require_once 'PHPUnit/Framework/TestCase.php';
+
+/**
+ * @see Zym_Version
+ */
+require_once 'Zym/Version.php';
+
+/**
+ * Version test
+ * 
+ * @author Geoffrey Tran
+ * @license http://www.zym-project.com//License New BSD License
+ * @category Zym
+ * @package Zym_Version
+ * @copyright Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ */
+class Zym_VersionTest extends PHPUnit_Framework_TestCase
+{    
+    /**
+     * Tests that version_compare() and its "proxy"
+     * Zym_Version::compareVersion() work as expected.
+     */
+    public function testVersionCompare()
+    {
+        $expect = -1;
+        // unit test breaks if ZF version > 1.x
+        for ($i=0; $i <= 1; $i++) {
+            for ($j=0; $j < 10; $j++) {
+                for ($k=0; $k < 20; $k++) {
+                    foreach (array('PR', 'dev', 'alpha', 'beta', 'RC', 'RC1', 'RC2', 'RC3', '', 'pl') as $rel) {
+                        $ver = "$i.$j.$k$rel";
+                        if ($ver === Zym_Version::VERSION
+                            || "$i.$j.$k-$rel" === Zym_Version::VERSION
+                            || "$i.$j.$k.$rel" === Zym_Version::VERSION
+                            || "$i.$j.$k $rel" === Zym_Version::VERSION) {
+
+                            if ($expect != -1) {
+                                $this->fail("Unexpected double match for Zym_Version::VERSION ("
+                                    . Zym_Version::VERSION . ")");
+                            }
+                            else {
+                                $expect = 1;
+                            }
+                        } else {
+                            $this->assertSame(Zym_Version::compareVersion($ver), $expect,
+                                "For version '$ver' and Zym_Version::VERSION = '"
+                                . Zym_Version::VERSION . "': result=" . (Zym_Version::compareVersion($ver))
+                                . ', but expected ' . $expect);
+                        }
+                    }
+                }
+            }
+        }
+        
+        if ($expect === -1) {
+            $this->fail('Unable to recognize Zym_Version::VERSION ('. Zym_Version::VERSION . ')');
+        }
+    }
+}
