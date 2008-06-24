f4ccb09a5f01fdbc643db633c6f691239cb763f2
SpotSec <SpotSec@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-06-24 22:08:45 +0000

    Seems like a good idea doesn't it? Perhaps we should make it so that vars are protected instead?
    git-svn-id: https://zym.googlecode.com/svn/trunk@448 242cd0b0-a150-0410-b6ef-417cb531347b

--- library/Zym/Controller/Action/Abstract.php
+++ library/Zym/Controller/Action/Abstract.php
@@ -31,6 +31,46 @@ require_once 'Zend/Controller/Action.php';
 abstract class Zym_Controller_Action_Abstract extends Zend_Controller_Action
 {
     /**
+     * Class constructor
+     *
+     * The request and response objects should be registered with the
+     * controller, as should be any additional optional arguments; these will be
+     * available via {@link getRequest()}, {@link getResponse()}, and
+     * {@link getInvokeArgs()}, respectively.
+     *
+     * When overriding the constructor, please consider this usage as a best
+     * practice and ensure that each is registered appropriately; the easiest
+     * way to do so is to simply call parent::__construct($request, $response,
+     * $invokeArgs).
+     *
+     * After the request, response, and invokeArgs are set, the
+     * {@link $_helper helper broker} is initialized.
+     *
+     * Finally, {@link init()} is called as the final action of
+     * instantiation, and may be safely overridden to perform initialization
+     * tasks; as a general rule, override {@link init()} instead of the
+     * constructor to customize an action controller's instantiation.
+     *
+     * @param Zend_Controller_Request_Abstract $request
+     * @param Zend_Controller_Response_Abstract $response
+     * @param array $invokeArgs Any additional invocation arguments
+     * @return void
+     */
+    public function __construct(Zend_Controller_Request_Abstract $request, Zend_Controller_Response_Abstract $response, array $invokeArgs = array())
+    {
+        parent::__construct($request, $response, $invokeArgs);
+    
+        if (isset($this->contexts)) {
+            // Init Contexts
+            $this->getHelper('ContextSwitch')->initContext();
+        }
+        
+        if (isset($this->ajaxable)) {
+            // Init AjaxContexts
+            $this->getHelper('AjaxContext')->initContext();
+        }
+    }
+    /**
      * Get the view object
      *
      * @return Zend_View
