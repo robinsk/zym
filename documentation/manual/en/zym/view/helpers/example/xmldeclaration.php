e89d23ee0e9704b9dd11d05562f3331a0a0bac66
SpotSec <SpotSec@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-06-26 08:53:17 +0000

    docs
    git-svn-id: https://zym.googlecode.com/svn/trunk@453 242cd0b0-a150-0410-b6ef-417cb531347b

---
+++ documentation/manual/en/zym/view/helpers/example/xmldeclaration.php
@@ -0,0 +1,15 @@
+<?= $this->xmlDeclaration() ?>
+<html>
+[<?xml version="1.0" encoding="UTF-8" ?>]
+
+<?= $this->xmlDeclaration('1.0') ?>
+<html>
+[<?xml version="1.0" encoding="UTF-8" ?>]
+
+<?= $this->xmlDeclaration('1.1', 'UTF-16') ?>
+<html>
+[<?xml version="1.0" encoding="UTF-16" ?>]
+
+<?= $this->xmlDeclaration('1.1', 'UTF-16', 'yes') ?>
+<html>
+[<?xml version="1.0" encoding="UTF-16" standalone="yes" ?>]
