24fa672a48358343791cec34f06a184002483a70
SpotSec <SpotSec@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-07-07 22:43:09 +0000

    Deprecation of Object helpers
    git-svn-id: https://zym.googlecode.com/svn/trunk@479 242cd0b0-a150-0410-b6ef-417cb531347b

---
+++ deprecated/tests/Zym/AllTests.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Zym
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @author     Geoffrey Tran
+ * @category   Zym_Tests
+ * @package    Zym
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license    http://www.zym-project.com/license    New BSD License
+ */
+
+set_include_path(dirname(__FILE__)  . '/../' . PATH_SEPARATOR
+                . dirname(__FILE__) . '/../../library' . PATH_SEPARATOR
+                . dirname(__FILE__) . '/../../incubator/library' . PATH_SEPARATOR . get_include_path());
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Zym_AllTests::main');
+}
+
+/**
+ * @see PHPUnit_TextUI_TestRunner
+ */
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+/**
+ * @see PHPUnit_Framework_TestSuite
+ */
+require_once 'PHPUnit/Framework/TestSuite.php';
+
+if (PHPUnit_MAIN_METHOD == 'Zym_AllTests::main') {
+    Zym_AllTests::main();
+}
+
+/**
+ * AllTests for Zym
+ *
+ * @author     Geoffrey Tran
+ * @category   Zym_Tests
+ * @package    Zym
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license    http://www.zym-project.com/license    New BSD License
+ */
+class Zym_AllTests
+{
+    /**
+     * Main()
+     *
+     * @return void
+     */
+    public static function main()
+    {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+    
+    /**
+     * Suite
+     *
+     * @return PHPUnit_Framework_TestSuite
+     */
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('Zym Framework - Core');
+        $suite->addTestFiles(glob(dirname(__FILE__) . '/*Suite.php'));
+        return $suite;
+    }
+}
