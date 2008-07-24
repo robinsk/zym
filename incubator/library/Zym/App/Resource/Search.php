f6fc8cce05706a24bd4e673c2cc7b029285d3a9d
norm2782 <norm2782@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-07-24 07:15:13 +0000

    Added Zym_Search resource for Zym_App
    git-svn-id: https://zym.googlecode.com/svn/trunk@597 242cd0b0-a150-0410-b6ef-417cb531347b

---
+++ incubator/library/Zym/App/Resource/Search.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Zym Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @category Zym
+ * @package Zym_App
+ * @subpackage Resource
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ * @license http://www.zym-project.com/license New BSD License
+ */
+
+/**
+ * @see Zym_App_Resource_Abstract
+ */
+require_once 'Zym/App/Resource/Abstract.php';
+
+/**
+ * @see Zym_Search_Lucene_Index
+ */
+require_once 'Zym/Search/Lucene/Index.php';
+
+/**
+ * Search
+ * 
+ * @author Jurrien Stutterheim
+ * @license http://www.zym-project.com/license New BSD License
+ * @category Zym
+ * @package Zym_App
+ * @subpackage Resource
+ * @copyright  Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ */
+class Zym_App_Resource_Search extends Zym_App_Resource_Abstract
+{
+    /**
+     * Default config
+     *
+     * @var array
+     */
+    protected $_defaultConfig = array(
+        Zym_App::ENV_DEFAULT => array(
+            'indexer' => array(
+                'defaults' => array(
+                    'indexpath' => null,
+                    'resultsetlimit' => 0
+                ),
+                /*
+                'indexes' => array(
+                    array(
+                        'indexpath' => '',
+                        'usedefaultpath' => true,
+                        'create' => true,
+                        'idkey' => ''
+                    ), ...
+                ),
+                */
+            )
+        )
+    );
+
+    /**
+     * Setup db
+     *
+     */
+    public function setup(Zend_Config $config)
+    {
+        // Get resource config
+        $indexerConfig = $config->indexer;
+        
+        Zym_Search_Lucene::setDefaultIndexPath($indexerConfig->defaults->indexpath);
+        Zym_Search_Lucene::setDefaultResultSetLimit($indexerConfig->defaults->resultsetlimit);
+                
+        foreach ($indexerConfig->indexes as $index) {
+            if (!isset($index->indexpath)) {
+                require_once 'Zym/App/Resource/Exception.php';
+                
+                throw new Zym_App_Resource_Exception('No index path set for the current index.');
+            }
+            
+            $indexpath = $index->indexpath;
+            
+            $useDefaultPath = isset($index->usedefaultpath) ? $index->usedefaultpath : true;
+            $createIfNotExists = isset($index->create) ? $index->create : true;
+            
+            $index = Zym_Search_Lucene::factory($index->indexpath, $useDefaultPath, $createIfNotExists);
+            
+            if (isset($index->idkey)) {
+                $index->setIdKey($index->idKey);
+            }
+        }
+    }
+}
