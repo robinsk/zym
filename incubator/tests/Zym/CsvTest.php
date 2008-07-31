6c27529cb54f57c6fc52adf85ff1c02e2b38d2b8
potatobob <potatobob@242cd0b0-a150-0410-b6ef-417cb531347b>
2008-07-31 06:10:36 +0000

    git-svn-id: https://zym.googlecode.com/svn/trunk@644 242cd0b0-a150-0410-b6ef-417cb531347b

--- incubator/tests/Zym/CsvTest.php
+++ incubator/tests/Zym/CsvTest.php
@@ -1,35 +1,65 @@
 <?php
-require_once 'trunk/incubator/library/Zym/Csv.php';
+/**
+ * Zym Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @category  Zym_Tests
+ * @package   Zym_Csv
+ * @license   http://www.zym-project.com/license    New BSD License
+ * @copyright Copyright (c) 2008 Zym. (http://www.zym-project.com/)
+ */
+
+/**
+ * @see PHPUnite_Framework_TestCase
+ */
 require_once 'PHPUnit/Framework/TestCase.php';
 
 /**
- * Zym_Csv test case.
+ * @see Zym_Csv
+ */
+require_once 'Zym/Csv.php';
+
+/**
+ * Tests the class Zym_Csv
+ *
+ * @category  Zym_Tests
+ * @package   Zym_Csv
+ * @license   http://www.zym-project.com/license    New BSD License
+ * @copyright Copyright (c) 2008 Zym. (http://www.zym-project.com/)
  */
 class Zym_CsvTest extends PHPUnit_Framework_TestCase
 {
     /**
+     *
      * @var Zym_Csv
      */
     private $Zym_Csv_NoHeader;
     private $Zym_Csv_Header;
+
     /**
      * Prepares the environment before running a test.
+     *
+     * @return void
      */
-    protected function setUp ()
+    protected function setUp()
     {
-        parent::setUp();
         // TODO Auto-generated Zym_CsvTest::setUp()
-        $this->Zym_Csv_Header = new Zym_Csv('/Users/norm2782/Zend/workspaces/DefaultWorkspace/Zym/trunk/incubator/tests/Zym/Csv/TestCsv.csv');
-        $this->Zym_Csv_NoHeader = new Zym_Csv('/Users/norm2782/Zend/workspaces/DefaultWorkspace/Zym/trunk/incubator/tests/Zym/Csv/TestCsv.csv', ',', false);
+        $this->Zym_Csv_Header = new Zym_Csv(dirname(__FILE__) . '/Csv/TestCsv.csv');
+        $this->Zym_Csv_NoHeader = new Zym_Csv(dirname(__FILE__) . 'Csv/TestCsv.csv', ',', false);
     }
     /**
      * Cleans up the environment after running a test.
+     *
+     * @return void
      */
-    protected function tearDown ()
+    protected function tearDown()
     {
         // TODO Auto-generated Zym_CsvTest::tearDown()
         $this->Zym_Csv_NoHeader = null;
-        parent::tearDown();
     }
 
     public function testConstruct()
@@ -41,7 +71,7 @@ class Zym_CsvTest extends PHPUnit_Framework_TestCase
         }
 
         try {
-            $test = new Zym_Csv('/Users/norm2782/Zend/workspaces/DefaultWorkspace/Zym/trunk/incubator/tests/Zym/Csv/TestCsvNoRead.csv');
+            $test = new Zym_Csv(dirname(__FILE__) . '/Csv/TestCsvNoRead.csv');
         } catch (Zym_Csv_Exception_FileNotReadable $e) {
             $this->assertTrue($e != null);
         }
@@ -60,7 +90,7 @@ class Zym_CsvTest extends PHPUnit_Framework_TestCase
     /**
      * Tests Zym_Csv->current()
      */
-    public function testCurrentNoHeader ()
+    public function testCurrentNoHeader()
     {
         $current = $this->Zym_Csv_NoHeader->current();
 
@@ -70,7 +100,7 @@ class Zym_CsvTest extends PHPUnit_Framework_TestCase
         $this->Zym_Csv_NoHeader->rewind();
     }
 
-    public function testCurrentHeader ()
+    public function testCurrentHeader()
     {
         $current = $this->Zym_Csv_Header->current();
 
@@ -82,14 +112,14 @@ class Zym_CsvTest extends PHPUnit_Framework_TestCase
     /**
      * Tests Zym_Csv->key()
      */
-    public function testKey ()
+    public function testKey()
     {
         $this->assertEquals(0, $this->Zym_Csv_NoHeader->key());
     }
     /**
      * Tests Zym_Csv->next()
      */
-    public function testNext ()
+    public function testNext()
     {
         $this->Zym_Csv_NoHeader->rewind();
         $this->assertTrue($this->Zym_Csv_NoHeader->next());
@@ -97,7 +127,7 @@ class Zym_CsvTest extends PHPUnit_Framework_TestCase
     /**
      * Tests Zym_Csv->rewind()
      */
-    public function testRewind ()
+    public function testRewind()
     {
         $this->Zym_Csv_NoHeader->rewind();
         $this->assertEquals(0, $this->Zym_Csv_NoHeader->key());
@@ -105,7 +135,7 @@ class Zym_CsvTest extends PHPUnit_Framework_TestCase
     /**
      * Tests Zym_Csv->toArray()
      */
-    public function testToArray ()
+    public function testToArray()
     {
         $array = $this->Zym_Csv_NoHeader->toArray();
 
@@ -121,7 +151,7 @@ class Zym_CsvTest extends PHPUnit_Framework_TestCase
     /**
      * Tests Zym_Csv->valid()
      */
-    public function testValid ()
+    public function testValid()
     {
         $this->Zym_Csv_NoHeader->rewind();
         $this->assertTrue($this->Zym_Csv_NoHeader->valid());
